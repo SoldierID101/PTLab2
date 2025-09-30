88ff107d47febdec2c087090f70cea1eaf778dc3
Ivanov Ivan Ivanovich <Ivanov@example.com>
2025-09-30 18:24:19 +0300

    feat: finalize variant 8, update tests and README

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView
+from .models import Product
 
-from .models import Product, Purchase
 
 # Create your views here.
 def index(request):
@@ -11,11 +11,11 @@ def index(request):
     return render(request, 'shop/index.html', context)
 
 
-class PurchaseCreate(CreateView):
-    model = Purchase
-    fields = ['product', 'person', 'address']
-
-    def form_valid(self, form):
-        self.object = form.save()
-        return HttpResponse(f'Спасибо за покупку, {self.object.person}!')
+#class PurchaseCreate(CreateView):
+#    model = Purchase
+#    fields = ['product', 'person', 'address']
+#
+#    def form_valid(self, form):
+#        self.object = form.save()
+#        return HttpResponse(f'Спасибо за покупку, {self.object.person}!')
 
